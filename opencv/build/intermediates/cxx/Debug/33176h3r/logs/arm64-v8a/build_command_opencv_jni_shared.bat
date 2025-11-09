524d72e10f0654f2334da1883d4eb3710e4d8f2e
Mihan552 <mihopro1@gmail.com>
2025-11-09 19:14:48 +0400

    Удаление библиотеки OpenCV из репозитория

--- opencv/build/intermediates/cxx/Debug/33176h3r/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\Desktop\\ToothCheck_v\\opencv\\.cxx\\Debug\\33176h3r\\arm64-v8a" ^
-  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/33176h3r/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\User\\Desktop\\ToothCheck\\opencv\\.cxx\\Debug\\33176h3r\\x86_64" ^
-  opencv_jni_shared
